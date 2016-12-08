1f4fbbcf689eb176af912d5b09c315e8a83459a3
Ben Camburn <foxguy75@gmail.com>
2016-12-08 14:08:07 -0500

    Centered Text Boxes
    Centered Text Boxes and also added link to github in the top righ thand corner

--- main.elm
+++ main.elm
@@ -1,103 +1,169 @@
+module Main exposing (..)
+
 import Html exposing (..)
 import Html.Events exposing (..)
 import Html.Attributes exposing (..)
 import String exposing (..)
-
 import Material
 import Material.Textfield as Textfield
 import Material.Grid exposing (..)
 import Material.Options as Options exposing (css)
 import Material.Layout as Layout
+import Material.Icon as Icon
 
 
 main : Program Never Model Msg
 main =
-  Html.program { init = init , update = update , subscriptions = Material.subscriptions Mdl, view = view }
+    Html.program { init = init, update = update, subscriptions = Material.subscriptions Mdl, view = view }
+
+
 
 --Model
 
+
 type alias Model =
-  { userName : String
-  , passWord : String
-  , passWordAgain : String
-  , mdl : Material.Model
-  }
+    { userName : String
+    , passWord : String
+    , passWordAgain : String
+    , mdl : Material.Model
+    }
+
 
 init : ( Model, Cmd Msg )
-init = (Model "" "" "" Material.model, Cmd.none)
+init =
+    ( Model "" "" "" Material.model, Cmd.none )
+
+
 
 -- Update
 
+
 type Msg
-  = UpdateUser String
-  | UpdatePassword String
-  | UpdatePasswordAgain String
-  | Mdl (Material.Msg Msg)
+    = UpdateUser String
+    | UpdatePassword String
+    | UpdatePasswordAgain String
+    | Mdl (Material.Msg Msg)
 
-update : Msg -> Model -> (Model, Cmd Msg)
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    UpdateUser input ->
-      ({ model | userName = input }, Cmd.none )
+    case msg of
+        UpdateUser input ->
+            ( { model | userName = input }, Cmd.none )
 
-    UpdatePassword input ->
-      ({ model | passWord = input }, Cmd.none )
+        UpdatePassword input ->
+            ( { model | passWord = input }, Cmd.none )
 
-    UpdatePasswordAgain input ->
-      ({ model | passWordAgain = input }, Cmd.none)
+        UpdatePasswordAgain input ->
+            ( { model | passWordAgain = input }, Cmd.none )
 
-    Mdl message ->
-      Material.update message model
+        Mdl message ->
+            Material.update message model
+
+
 
 -- View
 
+
 view : Model -> Html Msg
 view model =
-  Layout.render Mdl model.mdl
-  [ Layout.fixedHeader
-  ]
-  { drawer = []
-  , header = [ myheader ]
-  , main = [ viewbody model ]
-  , tabs = ([],[])
-  }
+    Layout.render Mdl
+        model.mdl
+        []
+        { drawer = []
+        , header = [ myheader ]
+        , main = [ viewbody model ]
+        , tabs = ( [], [] )
+        }
+
+
+boxed : List (Html.Attribute Msg)
+boxed =
+    [ style
+        [ ( "margin", "auto" )
+        , ( "width", "50%" )
+        , ( "padding", "1%" )
+        ]
+    ]
 
 
 viewbody : Model -> Html Msg
 viewbody model =
-  div []
-  [ Textfield.render Mdl [0] model.mdl
-    [ Textfield.label "User Name"
-    , Textfield.floatingLabel
-    , Textfield.onInput UpdateUser
-    ]
-    , passwordView model
-    , passwordConfirmView model
-  ]
+    div
+        boxed
+        [ div
+            boxed
+            [ Textfield.render Mdl
+                [ 0 ]
+                model.mdl
+                [ Textfield.label "User Name"
+                , Textfield.floatingLabel
+                , Textfield.onInput UpdateUser
+                ]
+            ]
+        , passwordView model
+        , passwordConfirmView model
+        ]
+
 
 passwordView : Model -> Html Msg
 passwordView model =
-  div []
-  [ Textfield.render Mdl [1] model.mdl
-    [ Textfield.label "Password"
-    , Textfield.floatingLabel
-    , Textfield.password
-    , Textfield.onInput UpdatePassword
-    ]
-  ]
+    div
+        boxed
+        [ Textfield.render Mdl
+            [ 1 ]
+            model.mdl
+            [ Textfield.label "Password"
+            , Textfield.floatingLabel
+            , Textfield.password
+            , Textfield.onInput UpdatePassword
+            ]
+        ]
+
 
 passwordConfirmView : Model -> Html Msg
 passwordConfirmView model =
-  div []
-  [ Textfield.render Mdl [2] model.mdl
-    [ Textfield.label "Confirm Password"
-    , Textfield.floatingLabel
-    , Textfield.password
-    , Textfield.onInput UpdatePasswordAgain
-    ]
-  ]
+    div
+        boxed
+        [ Textfield.render Mdl
+            [ 2 ]
+            model.mdl
+            [ Textfield.label "Confirm Password"
+            , Textfield.floatingLabel
+            , Textfield.password
+            , Textfield.onInput UpdatePasswordAgain
+            ]
+        ]
+
 
 myheader : Html Msg
 myheader =
-  h3 []
-  [ text "Ben Blog" ]
+    div
+        []
+        [ h5
+            [ style
+                [ ( "margin-left", "8%" )
+                , ( "margin-right", "8%" )
+                , ( "float", "left" )
+                , ( "display", "inline-block" )
+                ]
+            ]
+            [ text "Ben Blog" ]
+        , h5
+            [ style
+                [ ( "margin-left", "8%" )
+                , ( "margin-right", "8%" )
+                , ( "float", "right" )
+                , ( "display", "inline-block" )
+                ]
+            ]
+            [ a
+                [ href "https://github.com/foxguy75"
+                , style
+                    [ ( "text-decoration", "none" )
+                    , ( "color", "white" )
+                    ]
+                ]
+                [ text "GitHub" ]
+            ]
+        ]
